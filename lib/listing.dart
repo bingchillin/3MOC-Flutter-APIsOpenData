20fb9b02a551a09c46efb0ae7ab4894f5cb39c5c
Meithal <hurin8888@gmail.com>
2023-06-14 21:07:07 +0200

    debut travail listing

--- lib/listing.dart
+++ lib/listing.dart
@@ -8,6 +8,40 @@ class Listing extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CityExplore Paris'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded
+            (
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Élément $index'),
+                  subtitle: Text('Sous-titre de l\'élément $index'),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      // Barre de navigation ---------------------------------------------------
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Accueil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favoris',
+          ),
+        ],
+      ),
+    );
   }
 }
