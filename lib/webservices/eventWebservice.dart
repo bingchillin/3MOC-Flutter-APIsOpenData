ba67276ea4eb915041b94de7badc24771de9257b
Pokoki <kennysu78@hotmail.com>
2023-07-10 14:17:04 +0200

    creat list event

--- lib/webservices/eventWebservice.dart
+++ lib/webservices/eventWebservice.dart
@@ -11,15 +11,21 @@ class EventWebServices{
       final response = await http.get(Uri.parse('https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=&lang=FR&rows=10&facet=date_start&facet=date_end&facet=address_name&facet=address_zipcode&facet=address_city&facet=pmr&facet=price_type&facet=access_type&facet=updated_at&facet=programs'));
       switch(response.statusCode){
         case 200:
-          Map<String, dynamic> mapResponse = json.decode(response.body);
+          Map<String, dynamic>? mapResponse = json.decode(response.body) as Map<String, dynamic>?;
+
+          if(mapResponse == null){
+            return [];
+          }
+
           final events = mapResponse['records'].cast<Map<String, dynamic>>();
           return events.map<Event>((json) => Event.fromJson(json)).toList();
         default:
           throw Exception('Failed to load events');
       }
     }catch(error){
-      debugPrint('Error getting all events');
+      debugPrint('Error getting all events $error');
       rethrow;
     }
   }
 }
+
